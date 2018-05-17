a06fb89c63b30855bd8fb24d2e8f0d56cf7e3f17
Mick Sheahan <R3dCobbler@users.noreply.github.com>
2018-05-17 11:34:44 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,33 +1,42 @@
 library(shiny) 
 shinyServer(function(input, output) {
+       
     model <- reactive({
-        brushed_data <- brushedPoints(chicago, input$brush1,
-            xvar = "temp", yvar = "poll")
-        if(nrow(brushed_data) < 2){
-            return(NULL) 
-        }
-        lm(poll ~ temp, data = brushed_data) 
-    })
-    output$slopeOut <- renderText({ 
-        if(is.null(model())){
-            "No Model Found"
-        } else {
-            model()[[1]][2] 
-        }
-    })
-    output$intOut <- renderText({ 
-        if(is.null(model())){
-            "No Model Found"
-        } else {
-            model()[[1]][1] 
-        }
-    })
-    output$plot1 <- renderPlot({
-        plot(chicago$temp, chicago$poll, xlab = "Temperature", 
-             ylab = "Pollution", main = "Relationship between pollution and temperature", 
-             cex = 1.5, col = "dark green", pch = 16, bty = "n")
-        if(!is.null(model())){
-            abline(model(), col = "blue", lwd = 2) 
-        }
-    })
+    brushed_data <- brushedPoints(chicago, input$brush1,
+                            xvar = "Temperature", yvar = "Ozone")
+    if(nrow(brushed_data) < 2){
+      return(NULL)
+    }
+    lm(Ozone ~ Temperature, data = brushed_data)
+  })
+  output$slopeOut <- renderText({
+    if(is.null(model())){
+      "No Model Found"
+    } else {
+      model()[[1]][2]
+    }
+  })
+  
+  output$intOut <- renderText({
+    if(is.null(model())){
+      "No Model Found"
+    } else {
+      model()[[1]][1]
+    }
+  })
+  output$plot1 <- renderPlot({
+        
+        plot(chicago$Temperature, chicago$Ozone, xlab = "Temperature",
+         ylab = "Ozone", main = "Relationship between ozone levels and temperature",
+         col = "dark green", cex = 1.5, pch = 16, bty = "n")
+    if(!is.null(model())){
+      abline(model(), col = "blue", lwd = 2)
+    }
+  })
 })
+    
+ 
+    
+
+
+
--- ui.R
+++ ui.R
@@ -1,17 +1,24 @@
 library(dplyr)
+library(ggplot2)
 chicago <- readRDS("chicago.rds")
 
-chicago <- select(chicago, tmpd:pm25tmean2) %>%
-    rename(temp = tmpd, dewpoint = dptp, pm25 = pm25tmean2) %>%
-    mutate(pm25detrend = pm25 - mean(pm25, na.rm = TRUE)) %>% 
-    rename(poll = pm25detrend)
+chicago <- select(chicago, tmpd:pm25tmean2, pm10tmean2, o3tmean2) %>%
+    rename(Temperature = tmpd, Dewpoint = dptp, pm25 = pm25tmean2,
+           pm10 = pm10tmean2, o3 = o3tmean2) %>%
+    mutate(Fine_Particle = pm25 - mean(pm25, na.rm = TRUE),
+           Dust_Particle = pm10 - mean(pm10, na.rm = TRUE),
+           Ozone = o3 - mean(o3, na.rm = TRUE))
+    
 head(chicago)
 
-chicago <- chicago[!is.na(chicago$poll),]
-chicago <- chicago[!is.na(chicago$temp),]
+chicago <- chicago[!is.na(chicago$Fine_Particle),]
+chicago <- chicago[!is.na(chicago$Dust_Particle),]
+chicago <- chicago[!is.na(chicago$Ozone),]
+chicago <- chicago[!is.na(chicago$Temperature),]
 
 
 library(shiny)
+
 shinyUI(fluidPage( 
     tags$h1("Developing Data Products Course Project"),
     tags$p("This Shiny app looks at the relationship between temperature and fine particle pollution, measured on a daily basis between 1987 and 2005 in the city of Chicago, USA"),
@@ -20,29 +27,31 @@ shinyUI(fluidPage(
     tags$br(),
     tags$hr(),
     tags$h3("Instructions"),
-    tags$p("This example fits a linear model for the selected points and then draws a line of best fit for the resulting model."),
-    tags$p("Using your mouse, click and hold to 'draw' a selection of points in the chart below."),
-    tags$p("Once you have made a selection, you will see the regression line, and the associated slope and intercept values for that selection, in the box on the left."),
+    tags$p("The example below fits a linear model for the selected points and then draws a line of best fit for the resulting model."),
+    tags$p("Using your mouse, click and drag to 'draw' a selection of points in the chart below."),
+    tags$p("Once you have made a selection, you will see the regression line, and the associated slope and intercept values for that selection, in the widget box on the left."),
     tags$p("If you select an area with no points, you will see a 'No Model Found' message instead."),
     tags$p("If you do manage to get an error message, just right-click and hit 'reload'."),
     tags$p("Have fun!"),
     tags$hr(),
-
-    titlePanel("Explore the model"),
-    sidebarLayout(
-        sidebarPanel(
-            h3("Slope"),
-            textOutput("slopeOut"),
-            h3("Intercept"),
-            textOutput("intOut")
-        ),
-        mainPanel(
-            plotOutput("plot1", brush = brushOpts(
-                id = "brush1"
-            ))
-        )
+    
+    titlePanel("Explore Linear Models"),
+  sidebarLayout(
+    sidebarPanel(
+      h3("Slope"),
+      textOutput("slopeOut"),
+      h3("Intercept"),
+      textOutput("intOut")
+    ),
+    mainPanel(
+      plotOutput("plot1", brush = brushOpts(
+        id = "brush1"
+      ))
     )
+  )
 ))
+
+
 
     
 
